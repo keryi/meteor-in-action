aa00d8b26f5af67931d3dd218aa32934495dbf21
keryi <hti_32@yahoo.com>
2015-05-09 04:31:23 +0800

    Added template helper for products

--- myFridge/myFridge.js
+++ myFridge/myFridge.js
@@ -1,7 +1,11 @@
 Products = new Mongo.Collection('products')
 
 if (Meteor.isClient) {
-
+  Template.fridge.helpers({
+    products: function() {
+      return Products.find({ place: 'fridge' });
+    }
+  });
 }
 
 if (Meteor.isServer) {
